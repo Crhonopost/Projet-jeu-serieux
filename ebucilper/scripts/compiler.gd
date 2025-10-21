ce4b8ac125b3bca1ec8145725bd1d9e427ff19a6
Crhonopost <nath.souvignet@gmail.com>
2025-10-21 12:57:00 +0200

    while loop fixed

--- ebucilper/scripts/compiler.gd
+++ ebucilper/scripts/compiler.gd
@@ -29,41 +29,57 @@ func processInstructions(entryPoint: LogicResource, instructionIdx: int) -> Arra
 	elif(entryPoint is ListLogicResource):    
 		var instructionList : Array[Instruction]
 		
-		for child in entryPoint.childs:
-			instructionList.append_array(processInstructions(child, instructionIdx + 1))
-		
-		if(entryPoint is WhileLogicResource):
-			var jumpBackInstruction:= JumpToInstruction.new()
-			jumpBackInstruction.toIdx = instructionIdx
-			instructionList.append(jumpBackInstruction)
-			
-			var conditionExpressions: Array[Instruction] = []
-			var condiVarA = entryPoint.condition.variableA
-			var condiVarB = entryPoint.condition.variableB
-			if(condiVarA is OperationResource):
-				conditionExpressions.append_array(condiVarA.getInstructions("temp1"))
-				condiVarA = "temp1"
-			else:
-				condiVarA = condiVarA.value
-			if(condiVarB is OperationResource):
-				conditionExpressions.append_array(condiVarB.getInstructions("temp2"))
-				condiVarB = "temp2"
-			else:
-				condiVarB = condiVarB.value
-			
-			
-			var endLoopCondition := JumpToIfInstruction.new()
-			endLoopCondition.evaluateNot = true
-			endLoopCondition.condition.A = condiVarA
-			endLoopCondition.condition.B = condiVarB
-			endLoopCondition.condition.operator = entryPoint.condition.comparator
-			endLoopCondition.toIdx = instructionIdx + conditionExpressions.size() + instructionList.size() + 1
-			conditionExpressions.append(endLoopCondition)
-			
-			conditionExpressions.append_array(instructionList)
-			instructionList = conditionExpressions
-			
+		if entryPoint is WhileLogicResource:
+			instructionList.append_array(processWhile(entryPoint, instructionIdx))
+		else:
+			for child in entryPoint.childs:
+				instructionList.append_array(processInstructions(child, instructionIdx + instructionList.size()))
 		
 		res.append_array(instructionList)
+	return res
+
+
+func processWhile(whileLogic: WhileLogicResource, instructionIdx: int) -> Array[Instruction]:
+	var res : Array[Instruction]
+
+	var instructionList : Array[Instruction]
+	
+	################ jump condition #####################
+	var condiVarA = whileLogic.condition.variableA
+	var condiVarB = whileLogic.condition.variableB
+	if(condiVarA is OperationResource):
+		instructionList.append_array(condiVarA.getInstructions("temp1"))
+		condiVarA = "temp1"
+	else:
+		condiVarA = condiVarA.value
+	if(condiVarB is OperationResource):
+		instructionList.append_array(condiVarB.getInstructions("temp2"))
+		condiVarB = "temp2"
+	else:
+		condiVarB = condiVarB.value
+	
+	
+	var endLoopCondition := JumpToIfInstruction.new()
+	endLoopCondition.evaluateNot = true
+	endLoopCondition.condition.A = condiVarA
+	endLoopCondition.condition.B = condiVarB
+	endLoopCondition.condition.operator = whileLogic.condition.comparator
+	instructionList.append(endLoopCondition)
+	#####################################
+	
+	################# childs ####################
+	for child in whileLogic.childs:
+		instructionList.append_array(processInstructions(child, instructionIdx + instructionList.size()))
+	#####################################
+	
+	################# jump back ####################
+	var jumpBackInstruction:= JumpToInstruction.new()
+	jumpBackInstruction.toIdx = instructionIdx
+	instructionList.append(jumpBackInstruction)
+	#####################################
+	
+	endLoopCondition.toIdx = instructionIdx + instructionList.size()
+	
+	res.append_array(instructionList)
 	
 	return res
