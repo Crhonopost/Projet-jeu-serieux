0c0f9b6c1d231e009db0cc2cd1ecd73b01ecb227
Crhonopost <nath.souvignet@gmail.com>
2025-11-01 15:41:42 +0100

    fix level selection

--- ebucilper/scenes/UI/world/world.gd
+++ ebucilper/scenes/UI/world/world.gd
@@ -4,9 +4,22 @@ extends Node
 @onready var levelIntroductionScene : PackedScene = load("res://scenes/level/level_introduction.tscn")
 @onready var levelScene : PackedScene = load("res://scenes/level/current_level.tscn")
 
+@export var levels: Array[LevelResource]
+
 var game_instance: Node
 
-func _on_level_level_selected(level: LevelResource) -> void:
+func _enter_tree() -> void:
+	for child in $TextureRect/LevelsList/ScrollContainer/LevelList.get_children():
+		child.queue_free()
+	
+	var levelItemScene : PackedScene = load("res://scenes/UI/world/level.tscn")
+	for level in levels:
+		var l = levelItemScene.instantiate()
+		l.level_data = level
+		$TextureRect/LevelsList/ScrollContainer/LevelList.add_child(l)
+		l.connect("level_selected", _on_level_selected)
+
+func _on_level_selected(level: LevelResource) -> void:
 	$TextureRect.visible = false
 	
 	if(game_instance):
